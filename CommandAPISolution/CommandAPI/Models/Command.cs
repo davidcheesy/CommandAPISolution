b1348d386da47a5eb875e30a299a0f8672ab1fcf
davidcheesy <david.csizy@gmail.com>
2021-03-25 21:40:02 +0100

    Added Command Model to API Project

--- CommandAPISolution/CommandAPI/Models/Command.cs
+++ CommandAPISolution/CommandAPI/Models/Command.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,18 @@ namespace CommandAPI.Models
 {
     public class Command
     {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(250)]
+        public string HowTo { get; set; }
+
+        [Required]
+        public string Platform { get; set; }
+
+        [Required]
+        public string CommandLine { get; set; }
     }
 }
